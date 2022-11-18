6a9c28d6d9ca63583bec7deca455dbbe59110952
Eduardo Adams <eduardoadams2307@gmail.com>
2022-11-17 22:26:01 -0300

    update site

--- js/index.js
+++ js/index.js
@@ -22,3 +22,15 @@ window.addEventListener('scroll', ()=>{
 
 // FUNÇÃO IMAGENS
 
+var swiper = new Swiper(".swiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextE1: ".swiper-button-next",
+    prevE1: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  keyboard: true
+});
