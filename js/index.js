a1d04574b58572979606f8e9f9701f5d83f0cb13
Eduardo Adams <eduardoadams2307@gmail.com>
2022-11-22 06:03:41 -0300

    update site

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ window.addEventListener('scroll', ()=>{
 
 // SLIDE IMAGENS
 
-let swiper = new Swiper(".swiper", {
+let swiper = new swiper(".swiper", {
   cssMode: true,
   loop: true,
   navigation: {
@@ -34,3 +34,168 @@ let swiper = new Swiper(".swiper", {
   },
   keyboard: true
 });
+
+
+// TRANSIÇÃO DE IMAGEM HISTÓRIA
+
+let time = 2000,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll("#historia-slide img")
+    max = images.length;
+    
+
+function nextImage() {
+
+
+    images[currentImageIndex]
+    .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        nextImage()
+
+    }, time)
+}
+window.addEventListener("load", start) 
+
+
+// TRANSIÇÃO DE IMAGEM LITERATURA
+
+let liteTime = 2000,
+    literaturaImageIndex = 0,
+    literaturaImages = document
+                .querySelectorAll("#literatura-slide img")
+    literatura = literaturaImages.length;
+    
+
+function literaturaImage() {
+
+
+    literaturaImages[literaturaImageIndex]
+    .classList.remove("selected-Literatura")
+
+    literaturaImageIndex++
+
+    if(literaturaImageIndex >= LiteraturaMax)
+        literaturaImageIndex = 0
+
+    literaturaImages[literaturaImageIndex]
+        .classList.add("selected-Literatura")
+}
+
+function literatura() {
+    setInterval(() => {
+        literaturaImage()
+
+    }, liteTime)
+}
+window.addEventListener("load", literatura) 
+
+
+// TRANSIÇÃO DE IMAGEM CIENCIAS
+
+let cienciasTime = 2000,
+    cienciasImageIndex = 0,
+    cienciasImages = document
+                .querySelectorAll("#ciencias-slide img")
+    ciencias = cienciasImages.length;
+    
+
+function cienciasImage() {
+
+
+    cienciasImages[cienciasImageIndex]
+    .classList.remove("selected-ciencias")
+
+    cienciasImageIndex++
+
+    if(cienciasImageIndex >= cienciasMax)
+        cienciasImageIndex = 0
+
+    cienciasImages[cienciasImageIndex]
+        .classList.add("selected-ciencias")
+}
+
+function ciencias() {
+    setInterval(() => {
+        cienciasImage()
+
+    }, cienciasTime)
+}
+window.addEventListener("load", ciencias)  
+
+
+// TRANSIÇÃO DE IMAGEM FAUNA - FLORA
+
+let faunaTime = 2000,
+    faunaImageIndex = 0,
+    faunaImages = document
+                .querySelectorAll("#fauna-flora-slide img")
+    faunaMax = faunaImages.length;
+    
+
+function faunaImage() {
+
+
+    faunaImages[faunaImageIndex]
+    .classList.remove("selected-fauna-flora")
+
+    faunaImageIndex++
+
+    if(faunaImageIndex >= faunaMax)
+        faunaImageIndex = 0
+
+    faunaImages[faunaImageIndex]
+        .classList.add("selected-fauna-flora")
+}
+
+function faunaFlora() {
+    setInterval(() => {
+        faunaImage()
+
+    }, faunaTime)
+}
+window.addEventListener("load", faunaFlora) 
+
+
+// // TRANSIÇÃO DE IMAGEM VESTIMENTAS 
+
+// let vestimentasTime = 2000,
+//     vestimentasImageIndex = 0,
+//     vestimentasImages = document
+//                 .querySelectorAll("#vestimentos-slide img")
+//     vestimentasMax = vestimentasImages.length;
+    
+
+// function vestimentasImage() {
+
+
+//     vestimentasImages[vestimentasImageIndex]
+//     .classList.remove("selected-vestimentos")
+
+//     vestimentasImageIndex++
+
+//     if(vestimentasImageIndex >= vestimentasMax)
+//         vestimentasImageIndex = 0
+
+//     vestimentasImages[vestimentasImageIndex]
+//         .classList.add("selected-vestimentos")
+// }
+
+// function vestimentas() {
+//     setInterval(() => {
+//         vestimentasImage()
+
+//     }, vestimentasTime)
+// }
+// window.addEventListener("load", vestimentas) 
